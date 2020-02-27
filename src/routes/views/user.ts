508cde56a7c19c2a4e6ec4f6ec33c58082c29c5a
sakibcc <ud.k2007@gmail.com>
2020-02-27 23:03:58 +0800

    增加判断已登录

--- src/routes/views/user.ts
+++ src/routes/views/user.ts
@@ -3,15 +3,36 @@
  */
 
 import * as KoaRouter from 'koa-router'
+import { UserInfo } from '../../types'
+import { ParameterizedContext } from 'koa'
 
 const router = new KoaRouter()
 
+function getLoginInfo(
+  ctx: ParameterizedContext
+): {
+  isLogin: boolean
+  userInfo: UserInfo
+} {
+  let data = {
+    isLogin: false,
+    userInfo: null as UserInfo
+  }
+
+  if (ctx.session.userInfo) {
+    data.isLogin = true
+    data.userInfo = ctx.session.userInfo
+  }
+
+  return data
+}
+
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 export default router
