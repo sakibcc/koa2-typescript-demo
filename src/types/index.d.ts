b562db7cfaeb31c3ad7b73afeed399cb1bbbaa02
sakibcc <ud.k2007@gmail.com>
2020-02-27 23:26:46 +0800

    拓展type文件

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,9 +2,14 @@ import { BaseContext } from 'koa'
 import { Session } from 'koa-session'
 
 // declare
+declare module 'koa-session' {
+  interface Session {
+    userInfo: UserInfo
+  }
+}
 declare module 'koa' {
   interface BaseContext {
-    render(str: string, config: object): any
+    render(str: string, config: object): void
     session: Session
   }
 }
